a28ddba6ec5f63144ec22973c8967eff791a7283
ARYANPATILSOLOCODER11 <aryanpformal@gmail.com>
2024-03-16 21:10:36 +0530

    basic folders added : assets

--- android/app/src/main/java/com/cafelink/MainActivity.kt
+++ android/app/src/main/java/com/cafelink/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.cafelink
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -12,6 +12,14 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "CafeLink"
+
+    class MainActivity: ReactActivity() {
+  // ...
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
+  // ...
+}
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
